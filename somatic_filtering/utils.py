7ae8e61d554f0765dfd34eaa15c7352fd6455f5f
vladsaveliev <vladislav.sav@gmail.com>
2017-12-07 10:30:03 +1100

    Add get_loc() function; Start on patient analysis pipeline

--- somatic_filtering/utils.py
+++ somatic_filtering/utils.py
@@ -13,24 +13,34 @@ import yaml
 from ngs_utils.file_utils import open_gzipsafe
 
 
-def get_ref_path():
+def get_loc():
+    """ Depending on the machine name, return a dict conatining system-dependant paths 
+        to human reference genomes and extras
+    """
     hostname = socket.gethostname()
-    if hostname == '5180L-135800-M.local':
-        loc = 'local'
-        ref_loc = '/Users/vsaveliev/genomes/Hsapiens'
-
-    elif re.match(r'spartan.*\.hpc\.unimelb\.edu\.au', hostname):
-        loc = 'spartan'
-        ref_loc = '/home/vlad/bcbio/genomes/Hsapiens'
-
-    elif hostname.startswith('raijin'):
-        loc = 'raijin'
-        ref_loc = '/home/563/vs2870/g/bcbio/genomes/Hsapiens'
-
-    else:
-        ref_loc = ''
-
-    return ref_loc
+    for loc in [
+        dict(
+            name='spartan',
+            host_pattern=r'spartan.*\.hpc\.unimelb\.edu\.au',
+            hsapiens='/home/vlad/bcbio/genomes/Hsapiens',
+            extras='/data/projects/punim0010/local/share/extras/',
+        ),
+        dict(
+            name='raijin',
+            host_pattern=r'^raijin',
+            hsapiens='/home/563/vs2870/g/bcbio/genomes/Hsapiens',
+            extras='/g/data3/gx8/extras/',
+        ),
+        dict(
+            name='vlad',
+            host_pattern=r'^5180L-135800-M.local$',
+            hsapiens='/Users/vsaveliev/genomes/Hsapiens',
+            extras='/Users/vsaveliev/Analysis/umccrize/',
+        ),
+    ]:
+        if re.match(loc.host_pattern, hostname):
+            return loc
+    raise Exception('Could not find loc for hostname ' + loc)
 
 
 def get_sample_name(vcf_path):
@@ -45,3 +55,13 @@ def get_sample_name(vcf_path):
     raise ValueError
 
 
+BCINSTALL = "/data/projects/punim0010/local/share/bcbio/"
+BCRESULT = "/data/cephfs/punim0010/data/Results/Avner/WPT-013/final/"
+BCFINAL = "2017-10-19_WPT-013"
+BCPOST = "/data/cephfs/punim0010/projects/Hofmann_Explore/testrun"
+EXTRAS = "/data/projects/punim0010/local/share/extras/"
+
+# This information should come from the *.csv
+BCTUMOR = "WPT-013-organoid"
+BCNORMAL = "WPT-013-normal"
+BCBATCH = "batch1"
