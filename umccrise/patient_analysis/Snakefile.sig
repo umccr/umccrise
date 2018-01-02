ebc4a16043c1c4c237edbc97011c73cecefe4359
vladsaveliev <vladislav.sav@gmail.com>
2018-01-02 17:12:57 +1100

    Patient analysis: mutsig restrict to GiaB confident

--- umccrise/patient_analysis/Snakefile.sig
+++ umccrise/patient_analysis/Snakefile.sig
@@ -1,11 +1,37 @@
 from umccrise.patient_analysis import get_sig_rmd_file
 
 
+# Ensemble calls to be submitted to CGI. Sharing functionality does not seem to work at this point. Ensemble calls only include variants that `PASS` so no additional filtering required.
+# Finally, for the local analysis with MutationalPatterns generate UCSC-versions of the ensemble calls:
+rule cgi:
+    input:
+        'umccrised/{batch}/somatic/pcgr/ensemble.vcf.gz'
+    output:
+        'umccrised/{batch}/somatic/rstudio/ensemble-ucsf.vcf'
+    shell:
+        'gunzip -c {input}'
+        " | py -x \"x.replace('##contig=<ID=', '##contig=<ID=chr') if x.startswith('#') else 'chr' + x\""
+        " | py -x \"x.replace('chrMT', 'chrM')\""
+        ' | grep -v chrG > {output}'
+# All done; file needs to be submitted to CGI and PCGR manually for now. 
+
+
+rule restrict_to_confident:
+    input:
+        rules.cgi.output[0]
+    params:
+        regions = loc.truth_sets['giab'][run.genome_build]
+    output:
+        'umccrised/{batch}/somatic/rstudio/ensemble-ucsf-confident.vcf'
+    shell:
+        'bcftools {input} -L {params.regions} -Oz -o {output}'
+
+
 rule sig_rmd:
     input:
         af_freqs = rules.af_freqs.output[0],
         af_freqs_az300 = rules.af_freqs_az300.output[0],
-        vcf = rules.cgi.output[0]
+        vcf = rules.restrict_to_confident.output[0]
     params:
         sig_rmd = get_sig_rmd_file(),
         tumor_name = lambda wc: batch_by_name[wc.batch].tumor.name,
--- umccrise/utils.py
+++ umccrise/utils.py
@@ -17,7 +17,14 @@ def get_loc():
     """ Depending on the machine name, return a dict conatining system-dependant paths 
         to human reference genomes and extras
     """
-    Loc = collections.namedtuple('Loc', 'name host_pattern hsapiens extras panel_of_normals_dir')
+    Loc = collections.namedtuple('Loc',
+         'name '
+         'host_pattern '
+         'hsapiens '
+         'extras '
+         'panel_of_normals_dir '
+         'truth_sets ')
+
     hostname = socket.gethostname()
     for loc in [
         Loc(name='spartan',
@@ -25,19 +32,63 @@ def get_loc():
             hsapiens='/home/vlad/bcbio/genomes/Hsapiens',
             extras='/data/cephfs/punim0010/extras',
             panel_of_normals_dir='/data/cephfs/punim0010/extras/panel_or_normals',
+            truth_sets={
+                'mb': {
+                    'GRCh37': {
+                        'vcf': '/data/cephfs/punim0010/data/External/Reference/ICGC_MB/MB-benchmark.vcf.gz',
+                    }
+                },
+                'dream': {
+                    'GRCh37': {
+                        'vcf': 'GRCh37/validation/dream-syn3/truth_small_variants.vcf.gz',
+                        'bed': 'GRCh37/validation/dream-syn3/truth_regions.bed',
+                    }
+                },
+                'giab': {
+                    'GRCh37': {
+                        'vcf': 'GRCh37/validation/giab-NA12878/truth_small_variants.vcf.gz',
+                        'bed': 'GRCh37/validation/giab-NA12878/truth_regions.bed',
+                    }
+                }
+            },
         ),
         Loc(name='raijin',
             host_pattern=r'^raijin|(r\d\d\d\d$)',
             hsapiens='/home/563/vs2870/g/bcbio/genomes/Hsapiens',
             extras='/g/data3/gx8/extras',
             panel_of_normals_dir='/g/data3/gx8/extras/panel_or_normals',
+            truth_sets={},
         ),
         Loc(name='vlad',
             host_pattern=r'^5180L-135800-M.local$',
             hsapiens='/Users/vsaveliev/genomes/Hsapiens',
             extras='/Users/vsaveliev/Analysis/umccrize',
             panel_of_normals_dir='/Users/vsaveliev/Analysis/panel_of_normals/GRCh37/normals',
+            truth_sets={},
         ),]:
         if re.match(loc.host_pattern, hostname):
             return loc
     raise Exception('Could not find loc for hostname ' + hostname)
+
+
+
+
+"""
+All known truth sets
+"""
+truth_vcfs = {
+    'spartan': {
+        'mb': {
+            'GRCh37': {
+                'vcf': '/data/cephfs/punim0010/data/External/Reference/ICGC_MB/MB-benchmark.vcf.gz'
+            }
+        },
+        'dream': {
+            'GRCh37': {
+                'vcf': 'GRCh37/validation/dream-syn3/truth_small_variants.vcf.gz',
+                # 'bed': 'GRCh37/validation/dream-syn3/truth_regions.bed'
+            }
+        }
+    }
+}
+
