b2c728c54e959060383e859384fbc48dd09deab2
Roman Valls Guimera <brainstorm@nopcode.org>
2018-08-17 15:51:35 +1000

    Make sure cloud-init spawns a shell and that we do not overwrite the provisioning with same filename(s)

--- deploy/roles/brainstorm.umccrise-docker/files/bootstrap-instance.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -euxo pipefail
-
-export STACK="umccrise"
-
-wget https://raw.githubusercontent.com/umccr/workflows/master/$STACK/bootstrap-instance.sh && chmod +x bootstrap-instance.sh && ./boostrap-instance.sh
---
+++ deploy/roles/brainstorm.umccrise-docker/files/fetch-instance-bootstrap.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -euxo pipefail
+
+export STACK="umccrise"
+
+wget https://raw.githubusercontent.com/umccr/workflows/master/$STACK/bootstrap-instance.sh && chmod +x bootstrap-instance.sh && /bin/sh boostrap-instance.sh
