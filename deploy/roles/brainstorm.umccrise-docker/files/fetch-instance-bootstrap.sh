1952b5d497bd959aec520084caa3823c0462e314
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2018-09-04 10:05:24 +1000

    Fix typo in bootstrap script

--- deploy/roles/brainstorm.umccrise-docker/files/fetch-instance-bootstrap.sh
+++ deploy/roles/brainstorm.umccrise-docker/files/fetch-instance-bootstrap.sh
@@ -3,4 +3,4 @@ set -euxo pipefail
 
 export STACK="umccrise"
 
-wget https://raw.githubusercontent.com/umccr/workflows/master/$STACK/bootstrap-instance.sh && chmod +x bootstrap-instance.sh && /bin/sh boostrap-instance.sh
+wget https://raw.githubusercontent.com/umccr/workflows/master/$STACK/bootstrap-instance.sh && chmod +x bootstrap-instance.sh && /bin/sh bootstrap-instance.sh
