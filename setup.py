d676148f8c48c318961628be238fa31aafe335cf
vladsaveliev <vladislav.sav@gmail.com>
2018-11-02 11:33:12 +1100

    Clean up travis. Support "release"

--- setup.py
+++ setup.py
@@ -1,23 +1,29 @@
 #!/usr/bin/env python
-from setuptools import setup
+from setuptools import setup, find_packages
+from ngs_utils import setup_utils
 
-with open('VERSION.txt') as f:
-    version = f.read().strip().split('\n')[0]
+name = 'umccrise'
+
+version = setup_utils.get_cur_version(name)
 
 setup(
     name='umccrise',
     version=version,
     author='Vlad Saveliev',
+    author_email='vladislav.sav@gmail.com',
     description='UMCCRisation of bcbio-nextgen analysis results',
     keywords='bioinformatics',
+    url='https://github.com/umccr/umccrise',
     license='GPLv3',
-    packages=[
-        'umccrise',
-    ],
+    package_data={
+        name: setup_utils.find_package_files('', name)
+    },
+    packages=find_packages(),
     scripts=[
         'vendor/vcfToBedpe',
         'scripts/umccrise',
         'scripts/pcgr',
     ],
     include_package_data=True,
+    zip_safe=False,
 )
