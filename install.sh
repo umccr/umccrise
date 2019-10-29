66aaa7e0687dc371918c99ef9200ddd996ec5e5e
Roman Valls Guimera <brainstorm@nopcode.org>
2019-10-29 13:19:28 +1100

    Do not use /Users/romanvg/dev/umccr/umccrise when /Users/romanvg/dev/umccr/umccrise is already in the environment /cc @vladsaveliev

--- install.sh
+++ install.sh
@@ -13,22 +13,22 @@ else
     wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 fi
 bash miniconda.sh -b -p ./miniconda && rm miniconda.sh
-export PATH=$(pwd)/miniconda/bin:$PATH
+export PATH=$PWD/miniconda/bin:$PATH
 conda update conda
 
 ### Install environments
 ENV_NAME=umccrise
-conda env create --force -p $(pwd)/miniconda/envs/${ENV_NAME} --file umccrise/envs/umccrise.yml
-conda env create --force -p $(pwd)/miniconda/envs/${ENV_NAME}_hmf --file umccrise/envs/hmf.yml
+conda env create --force -p $PWD/miniconda/envs/${ENV_NAME} --file umccrise/envs/umccrise.yml
+conda env create --force -p $PWD/miniconda/envs/${ENV_NAME}_hmf --file umccrise/envs/hmf.yml
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    conda env create --force -p $(pwd)/miniconda/envs/${ENV_NAME}_pcgr --file umccrise/envs/pcgr_macos.yml
+    conda env create --force -p $PWD/miniconda/envs/${ENV_NAME}_pcgr --file umccrise/envs/pcgr_macos.yml
 else
-    conda env create --force -p $(pwd)/miniconda/envs/${ENV_NAME}_pcgr --file umccrise/envs/pcgr_linux.yml
+    conda env create --force -p $PWD/miniconda/envs/${ENV_NAME}_pcgr --file umccrise/envs/pcgr_linux.yml
 fi
 
-# Instead of `conda activate $(pwd)/miniconda/envs/${ENV_NAME}`:
-export PATH=$(pwd)/miniconda/envs/${ENV_NAME}/bin:$PATH
-export CONDA_PREFIX=$(pwd)/miniconda/envs/umccrise
+# Instead of `conda activate $PWD/miniconda/envs/${ENV_NAME}`:
+export PATH=$PWD/miniconda/envs/${ENV_NAME}/bin:$PATH
+export CONDA_PREFIX=$PWD/miniconda/envs/umccrise
 
 # Install master on top
 pip install -e umccrise
@@ -43,6 +43,6 @@ ENV_NAME=umccrise
 cat <<EOT > load_umccrise.sh
 unset PYTHONPATH
 unset PERL5LIB
-export PATH=$(pwd)/miniconda/envs/${ENV_NAME}/bin:$(pwd)/miniconda/bin:\$PATH
-export CONDA_PREFIX=$(pwd)/miniconda/envs/${ENV_NAME}
+export PATH=$PWD/miniconda/envs/${ENV_NAME}/bin:$PWD/miniconda/bin:\$PATH
+export CONDA_PREFIX=$PWD/miniconda/envs/${ENV_NAME}
 EOT
