0d56127af08bedcb25e4af34b3d3e6b2a90b4857
vladsaveliev <vladislav.sav@gmail.com>
2020-03-18 11:02:18 +1100

    Add multiqc background prep for Dragen

--- umccrise/multiqc/gold_standard/README.sh
+++ umccrise/multiqc/gold_standard/README.sh
@@ -1,6 +1,6 @@
-#############################
-### Prepare gold standard ###
-#############################
+#####################################
+### Prepare gold standard (bcbio) ###
+#####################################
 # ssh spa
 
 cd /data/cephfs/punim0010/data/Results/Tothill-A5/2018-08-11
@@ -28,4 +28,35 @@ done
 
 # Rename sample IDs in all files and filenames into target nice names (Alice, Bob, ...)
 python rename.py ${DIR_QCONLY} ${DIR_QCONLY}.renamed ${DIR_QCONLY}.renamed_hg38
+
+
+
+######################################
+### Prepare gold standard (DRAGEN) ###
+######################################
+# ssh rjn
+
+cd /g/data/gx8/extras/umccrise_multiqc_background
+iap files download gds://"umccr-primary-data-dev/results/*" .
+
+umccrise Alice -c -j30 -o umccrised/Alice multiqc
+umccrise Bob -c -j30 -o umccrised/Bob multiqc
+umccrise Chen -c -j30 -o umccrised/Chen multiqc
+umccrise Dakota -c -j30 -o umccrised/Dakota multiqc
+umccrise Elon -c -j30 -o umccrised/Elon multiqc
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
