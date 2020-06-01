bc175d5b679c99f7068daf3ad19c7cc4b2996c0c
vladsaveliev <vladislav.sav@gmail.com>
2020-06-01 17:48:27 +1000

    Update MultiQC background generation (now on raijin)

--- umccrise/multiqc/gold_standard/README.sh
+++ umccrise/multiqc/gold_standard/README.sh
@@ -1,18 +1,18 @@
 #####################################
 ### Prepare gold standard (bcbio) ###
 #####################################
-# ssh spa
-cd /data/cephfs/punim0010/data/Results/Tothill-A5/2018-08-11
+# ssh rjn
+cd /g/data/gx8/extras/umccrise_multiqc_background/bcbio
 
+# Run the recent version of umccrise:
 DIR=umccrised
-# Run recent version of umccrise:
-umccrise final -c -j30 -o ${DIR} multiqc -s E201,E199,E194,E190,E202
-
-cd making_background_qc
+source /g/data/gx8/extras/umccrise/load_umccrise.sh
+umccrise final -c -j30 -o ${DIR} -T multiqc -s E201 -s E199 -s E194 -s E190 -s E202
 
 # Subset filelist to QC files only:
 DIR_QCONLY=${DIR}.qconly
 mkdir ${DIR_QCONLY}
+touch ${DIR_QCONLY}/background_multiqc_filelist.txt
 for batch in E201 E199 E194 E190 E202 ; do
     cat ${DIR}/work/${batch}_*/multiqc_data/filelist.txt | grep -v gold_standard | grep ${batch}\
         >> ${DIR_QCONLY}/background_multiqc_filelist.txt
