bab7c8f151b50ebfe6d940b6facee05857f0e641
vladsaveliev <vladislav.sav@gmail.com>
2020-09-27 21:20:56 +1000

    BuildSpec: test in a single thread

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ else
     wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
     -O miniconda.sh && chmod +x miniconda.sh
 fi
-./miniconda.sh -b -p $PWD/miniconda && rm miniconda.sh
+bash miniconda.sh -b -p $PWD/miniconda && rm miniconda.sh
 export PATH=$PWD/miniconda/bin:$PATH
 conda config --set always_yes yes --set changeps1 no
 conda install -c conda-forge mamba
