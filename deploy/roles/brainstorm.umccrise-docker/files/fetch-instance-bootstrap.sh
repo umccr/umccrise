dc125d26d1951837d705d4e76b63c93aa0f3acc3
pdiakumis <peterdiakumis@gmail.com>
2022-09-29 17:32:57 +1000

    remove 4yr-old deploy/ + Vagrantfile files

--- deploy/roles/brainstorm.umccrise-docker/files/fetch-instance-bootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euxo pipefail
-
-STACK="umccrise"
-
-SCRIPT="bootstrap-instance.sh"
-
-wget https://raw.githubusercontent.com/umccr/workflows/master/$STACK/$SCRIPT -O "/$SCRIPT" && chmod +x "/$SCRIPT" && /bin/bash "/$SCRIPT"
